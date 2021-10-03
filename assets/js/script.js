2e542152d827c8960f2a6b93ee2b20a1e083f770
AS <asadrgilany7@gmail.com>
2021-10-03 15:52:05 -0400

    fetch and some fight sequence done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,197 +1,328 @@
 // Venusaur, Charizard, Blastoise, Mew, Dragonite, Gyarados (edited)
-var venusaurFront;
-var venusaurBack;
 
-var charizardFront;
-var charizardBack;
+var venusaurObj = {
+    health: 100,
+    attack: 20,
+    effective: 40,
+    noteffective: 5
+}
 
-var blastoiseFront;
-var blastoiseBack;
+var charizardObj = {
+    health: 100,
+    attack: 20,
+    effective: 40,
+    noteffective: 5
+}
 
-var mewFront;
-var mewBack;
+var blastoiseObj = {
+    health: 100,
+    attack: 20,
+    effective: 40,
+    noteffective: 5
+}
 
-var dragoniteFront;
-var dragoniteBack;
+var mewObj = {
+    health: 100,
+    attack: 20,
+    effective: 40,
+    noteffective: 5
+}
 
-var gyardosFront;
-var gyaradosBack;
+var dragoniteObj = {
+    health: 100,
+    attack: 20,
+    effective: 40,
+    noteffective: 5
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-    (document.querySelectorAll('.message .delete') || []).forEach(($delete) => {
-      const $message = $delete.parentNode;
-  
-      $delete.addEventListener('click', () => {
-        $message.parentNode.removeChild($message);
-      });
-    });
-  });
+var gyaradosObj = {
+    health: 100,
+    attack: 20,
+    effective: 40,
+    noteffective: 5
+}
 
-var fetchVenusaur = function() {
-    var apiUrl = "https://pokeapi.co/api/v2/pokemon/venusaur";
+var userPokeObj = {};
+
+var enemyPokeObj = {};
+
+var currentDiceRoll;
+
+//create fetch request to display the 6 pokemon
+var fetchPokemon = function(poke) {
+    var apiUrl = "https://pokeapi.co/api/v2/pokemon/" + poke;
 
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-                    var pokeName = data.species.name;
-                    console.log(pokeName);
-
-                    var pokeSpriteFrontUrl = data.sprites.front_shiny;
-                    var venusaurFront = pokeSpriteFrontUrl;
-                    console.log(venusaurFront);
-
-                    var pokeSpriteBackUrl = data.sprites.back_shiny;
-                    var venusaurBack = pokeSpriteBackUrl;
-                    console.log(venusaurBack);
-
-                    var newImgEl = $("<img/>");
-                    newImgEl.attr("src", venusaurFront);
-                    $(".poke").append(newImgEl);
+                    displayPokemon(data);
                 })
+            }
+            else {
+                // modal to show that the fetch didn't work
             }
         })
 }
 
-var fetchCharizard = function() {
-    var apiUrl = "https://pokeapi.co/api/v2/pokemon/charizard";
+var displayPokemon = function(data) {
+
+    var pokeName = data.name;
+    var front = data.sprites.front_shiny;
+    var back = data.sprites.back_shiny;
+
+    updateObjects(data);
+
+    var pokeHeader = $("<h2></h2>");
+    pokeHeader.text(pokeName);
+    $("#show-poke").append(pokeHeader);
+
+    var pokeImg = $("<img/>");
+    pokeImg.attr("src", front);
+    pokeImg.attr("id", "pokeId");
+    pokeHeader.append(pokeImg);
+
+}
+
+var updateObjects = function (data) {
+    if (data.name === "charizard") {
+        charizardObj.name = data.name;
+        charizardObj.front = data.sprites.front_shiny;
+        charizardObj.back = data.sprites.back_shiny;
+    }
+    else if (data.name === "venusaur") {
+        venusaurObj.name - data.name;
+        venusaurObj.front = data.sprites.front_shiny;
+        venusaurObj.back = data.sprites.back_shiny;
+    }
+    else if (data.name === "mew") {
+        mewObj.name = data.name;
+        mewObj.front = data.sprites.front_shiny;
+        mewObj.back = data.sprites.back_shiny;
+    }
+    else if (data.name === "dragonite") {
+        dragoniteObj.name = data.name;
+        dragoniteObj.front = data.sprites.front_shiny;
+        dragoniteObj.back = data.sprites.back_shiny;
+    }
+    else if (data.name === "blastoise") {
+        blastoiseObj.name = data.name;
+        blastoiseObj.front = data.sprites.front_shiny;
+        blastoiseObj.back = data.sprites.back_shiny;
+    }
+    else if (data.name === "gyarados") {
+        gyaradosObj.name = data.name;
+        gyaradosObj.front = data.sprites.front_shiny;
+        gyaradosObj.back = data.sprites.back_shiny;
+    }
+}
+
+var fetchAll = function() {
+    fetchPokemon("charizard");
+    fetchPokemon("venusaur");
+    fetchPokemon("blastoise");
+    fetchPokemon("mew");
+    fetchPokemon("dragonite");
+    fetchPokemon("gyarados");
+}
+
+// var fetchDice = async function(sides) {
+
+//     var apiUrl = "http://roll.diceapi.com/json/" + sides;
+
+//     fetch(apiUrl)
+//         .then(function(response) {
+//             if (response.ok) {
+//                 response.json().then(function(data) {
+//                     var result = data.dice[0].value;
+
+//                 })
+//             }
+//             else {
+//                 // insert modal for bad fetch
+//             }
+//         })
+// }
+
+var userAttack = function(sides) {
+    
+    var apiUrl = "http://roll.diceapi.com/json/" + sides;
 
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-                    var pokeName = data.species.name;
-                    console.log(pokeName);
+                    var dice = data.dice[0].value;
+                    
+                    if (dice === 1) {
+                        // miss attack
+                    }
+                    else if (dice === 2) {
+                        // regular attack
+                        enemyPokeObj.health -= userPokeObj.attack;
+                        console.log(enemyPokeObj.health);
+                        checkHealth();
+                    }
+                    else if (dice === 3) {
+                        // super effective attack
+                        enemyPokeObj.health -= userPokeObj.effective;
+                        console.log(enemyPokeObj.health);
+                        checkHealth();
+                    }
+                    else if (dice === 4) {
+                        // not very effective attack
+                        enemyPokeObj.health -= userPokeObj.noteffective;
+                        console.log(enemyPokeObj.health);
+                        checkHealth();
+                    }
 
-                    var pokeSpriteFrontUrl = data.sprites.front_shiny;
-                    var charizardFront = pokeSpriteFrontUrl;
-                    console.log(charizardFront);
-
-                    var pokeSpriteBackUrl = data.sprites.back_shiny;
-                    var charizardBack = pokeSpriteBackUrl;
-                    console.log(charizardBack);
-
-                    var newImgEl = $("<img/>");
-                    newImgEl.attr("src", charizardFront);
-                    $(".poke").append(newImgEl);
                 })
             }
+            else {
+                // insert modal for bad fetch
+            }
         })
+    
 }
 
-var fetchBlastoise = function() {
-    var apiUrl = "https://pokeapi.co/api/v2/pokemon/blastoise";
+var enemyAttack = function(sides) {
+
+    var apiUrl = "http://roll.diceapi.com/json/" + sides;
 
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-                    var pokeName = data.species.name;
-                    console.log(pokeName);
+                    var dice = data.dice[0].value;
 
-                    var pokeSpriteFrontUrl = data.sprites.front_shiny;
-                    var blastoiseFront = pokeSpriteFrontUrl;
-                    console.log(blastoiseFront);
+                    if (dice === 1) {
+                        // miss attack
+                    }
+                    else if (dice === 2) {
+                        // regular attack
+                        userPokeObj.health -= enemyPokeObj.attack;
+                        console.log(enemyPokeObj.health);
+                        checkHealth();
+                    }
+                    else if (dice === 3) {
+                        // super effective attack
+                        userPokeObj.health -= enemyPokeObj.effective;
+                        console.log(enemyPokeObj.health);
+                        checkHealth();
+                    }
+                    else if (dice === 4) {
+                        // not very effective attack
+                        userPokeObj.health -= enemyPokeObj.noteffective;
+                        console.log(enemyPokeObj.health);
+                        checkHealth();
+                    }
 
-                    var pokeSpriteBackUrl = data.sprites.back_shiny;
-                    var blastoiseBack = pokeSpriteBackUrl;
-                    console.log(blastoiseBack);
-
-                    var newImgEl = $("<img/>");
-                    newImgEl.attr("src", blastoiseFront);
-                    $(".poke").append(newImgEl);
                 })
+            }
+            else {
+                // insert modal for bad fetch
             }
         })
 }
 
-var fetchMew = function() {
-    var apiUrl = "https://pokeapi.co/api/v2/pokemon/mew";
+var checkHealth = function() {
 
-    fetch(apiUrl)
-        .then(function(response) {
-            if (response.ok) {
-                response.json().then(function(data) {
-                    var pokeName = data.species.name;
-                    console.log(pokeName);
+    if (userPokeObj.health <= 0) {
+        //game over you lose
 
-                    var pokeSpriteFrontUrl = data.sprites.front_shiny;
-                    var mewFront = pokeSpriteFrontUrl;
-                    console.log(mewFront);
-
-                    var pokeSpriteBackUrl = data.sprites.back_shiny;
-                    var mewBack = pokeSpriteBackUrl;
-                    console.log(mewBack);
-
-                    var newImgEl = $("<img/>");
-                    newImgEl.attr("src", mewFront);
-                    $(".poke").append(newImgEl);
-                })
-            }
-        })
+    }
+    else if (enemyPokeObj.health <= 0) {
+        // game over you win!
+    }
 }
 
-var fetchDragonite = function() {
-    var apiUrl = "https://pokeapi.co/api/v2/pokemon/dragonite";
+var selectPokemon = function(event) {
 
-    fetch(apiUrl)
-        .then(function(response) {
-            if (response.ok) {
-                response.json().then(function(data) {
-                    var pokeName = data.species.name;
-                    console.log(pokeName);
+    var target = event.target;
+    var parent = target.parentElement;
+    $(target).remove();
+    $(parent).remove();
 
-                    var pokeSpriteFrontUrl = data.sprites.front_shiny;
-                    var dragoniteFront = pokeSpriteFrontUrl;
-                    console.log(dragoniteFront);
+    $("#user-poke").append(target);
 
-                    var pokeSpriteBackUrl = data.sprites.back_shiny;
-                    var dragoniteBack = pokeSpriteBackUrl;
-                    console.log(dragoniteBack);
 
-                    var newImgEl = $("<img/>");
-                    newImgEl.attr("src", dragoniteFront);
-                    $(".poke").append(newImgEl);
-                })
-            }
-        })
+    $("#show-poke").off("click");
+
+    var imgSrc = $(target).attr("src");
+    if (imgSrc === charizardObj.front) {
+        userPokeObj = charizardObj;
+    }
+    else if (imgSrc === venusaurObj.front) {
+        userPokeObj = venusaurObj;
+    }
+    else if (imgSrc === mewObj.front) {
+        userPokeObj = mewObj;
+    }
+    else if (imgSrc === blastoiseObj.front) {
+        userPokeObj = blastoiseObj;
+    }
+    else if (imgSrc === dragoniteObj.front) {
+        userPokeObj = dragoniteObj;
+    }
+    else if (imgSrc === gyaradosObj.front) {
+        userPokeObj = gyaradosObj;
+    }
+
+    $("#show-poke").on("click", selectEnemy);
+    console.log(userPokeObj);
 }
 
-var fetchGyarados = function() {
-    var apiUrl = "https://pokeapi.co/api/v2/pokemon/gyarados";
+var selectEnemy = function(event) {
 
-    fetch(apiUrl)
-        .then(function(response) {
-            if (response.ok) {
-                response.json().then(function(data) {
-                    var pokeName = data.species.name;
-                    console.log(pokeName);
+    var target = event.target;
+    var parent = target.parentElement;
 
-                    var pokeSpriteFrontUrl = data.sprites.front_shiny;
-                    var gyardosFront = pokeSpriteFrontUrl;
-                    console.log(gyardosFront);
+    $(target).remove();
+    $(parent).remove();
 
-                    var pokeSpriteBackUrl = data.sprites.back_shiny;
-                    var gyaradosBack = pokeSpriteBackUrl;
-                    console.log(gyaradosBack);
+    $("#enemy-poke").append(target);
 
-                    var newImgEl = $("<img/>");
-                    newImgEl.attr("src", gyardosFront);
-                    $(".poke").append(newImgEl);
-                })
-            }
-            
-        })
+    $("#show-poke").off("click");
+    $("#show-poke").empty();
+
+    var imgSrc = $(target).attr("src");
+    if (imgSrc === charizardObj.front) {
+        enemyPokeObj = charizardObj;
+    }
+    else if (imgSrc === venusaurObj.front) {
+        enemyPokeObj = venusaurObj;
+    }
+    else if (imgSrc === mewObj.front) {
+        enemyPokeObj = mewObj;
+    }
+    else if (imgSrc === blastoiseObj.front) {
+        enemyPokeObj = blastoiseObj;
+    }
+    else if (imgSrc === dragoniteObj.front) {
+        enemyPokeObj = dragoniteObj;
+    }
+    else if (imgSrc === gyaradosObj.front) {
+        enemyPokeObj = gyaradosObj;
+    }
+    console.log(enemyPokeObj);
+
+    fightSequence();
 }
 
-fetchGyarados();
+var fightSequence = function() {
+    
+    for (var i = 0; i < 20; i++) {
+        userAttack("d4");
+        enemyAttack("d4");
+    }
+}
 
-fetchMew();
 
-fetchDragonite();
 
-fetchBlastoise();
+fetchAll();
+$("#to-battle").on("click", fetchAll);
 
-fetchVenusaur();
 
-fetchCharizard();
+$("#show-poke").on("click", selectPokemon);
+
+
